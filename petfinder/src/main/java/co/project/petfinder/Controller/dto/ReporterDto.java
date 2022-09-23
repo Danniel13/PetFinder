8a04e18c0fa40df7a1a1e965e9142ae274443821
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-22 22:23:51 -0500

    changes

---
+++ petfinder/src/main/java/co/project/petfinder/Controller/dto/ReporterDto.java
@@ -0,0 +1,19 @@
+package co.project.petfinder.Controller;
+
+
+public class ReporterDto {
+private String petName;
+private String petOwner;
+private Integer phonenumber;
+private String breed;
+private String email;
+private String instagram;
+private String dateOfLost;
+private String city;
+private String description;
+private String imageUrl;  
+
+
+
+
+}
