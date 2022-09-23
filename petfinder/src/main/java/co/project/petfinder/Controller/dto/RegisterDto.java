9be2b6cb5ba7169ebd5d092a2d799b975d18af8d
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-22 23:14:02 -0500

    Add entity

--- petfinder/src/main/java/co/project/petfinder/Controller/dto/RegisterDto.java
+++ petfinder/src/main/java/co/project/petfinder/Controller/dto/RegisterDto.java
@@ -8,8 +8,8 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RegisterDto {
-  private String username;
-  private String password;
   private String name;
+  private String lastname;
+  private String password;
   private String email;
 }
---
+++ petfinder/src/main/java/co/project/petfinder/model/entity/User.java
@@ -0,0 +1,33 @@
+package co.project.petfinder.model.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "user")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+  
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  @Column(name = "user_id")
+  private String name;
+  private String lastname;
+  private String password;
+  
+  @Column (unique = true)
+  private String email; 
+
+}
