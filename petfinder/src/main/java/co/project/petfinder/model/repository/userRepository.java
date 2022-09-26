ddfe833779b4e4c65f32ff229f4a9e7af67c767b
Carlos Adolfo Ovalle Arias <covalle97188@universidadean.edu.co>
2022-09-25 23:23:31 -0500

    backend2

---
+++ petfinder/src/main/java/co/project/petfinder/model/repository/userRepository.java
@@ -0,0 +1,15 @@
+package co.project.petfinder.model.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.project.petfinder.model.entity.User;
+
+public interface userRepository extends JpaRepository<User, String> {
+    
+    Optional<User> findByUsernameAndPasswordAndActiveIsTrue(String username, String password);
+
+    Optional<User> findByEmail(String email);
+
+}
