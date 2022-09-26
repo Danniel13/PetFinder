abc96695215effed2fd87e4e0149fce76229e431
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-26 15:01:13 -0500

    comments

--- petfinder/src/main/java/co/project/petfinder/model/entity/User.java
+++ petfinder/src/main/java/co/project/petfinder/model/entity/User.java
@@ -13,7 +13,7 @@ import javax.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
+//Backend y Base de datos!
 @Entity
 @Table(name = "user")
 @Data
