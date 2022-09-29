d295049d6152807c6f5f0531bd4fa2b4f985421f
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-09-28 23:20:55 -0500

    Primer prueba register

--- petfinder/src/main/java/co/project/petfinder/service/impl/ReporterServiceImpl.java
+++ petfinder/src/main/java/co/project/petfinder/service/impl/ReporterServiceImpl.java
@@ -10,23 +10,23 @@ import co.project.petfinder.model.repository.ReporterRepository;
 import co.project.petfinder.service.ReporterService;
 import lombok.AllArgsConstructor;
 
-@Service
-@AllArgsConstructor
-public class ReporterServiceImpl implements ReporterService {
+// @Service
+// @AllArgsConstructor
+public class ReporterServiceImpl  {
 
-    private final ReporterRepository ReporterRepository;
+//     private final ReporterRepository ReporterRepository;
     
-@Override
-//Creación super clase getReportes en ReporterService.java
-    public List<ReporterDto> getReportes() {
-        // ToDo cambiarlo a Pageable para seleccionar de a 9 animales reportados
-        var reportes = ReporterRepository.findAll(Sort.by("name"));
+// @Override
+// //Creación super clase getReportes en ReporterService.java
+//     public List<ReporterDto> getReportes() {
+//         // ToDo cambiarlo a Pageable para seleccionar de a 9 animales reportados
+//         var reportes = ReporterRepository.findAll(Sort.by("name"));
 
-        //Modificación de nombre de atributos Getter
-        return reportes.stream()
-            .map(rep -> new ReporterDto(rep.getId(), rep.getPetName(), rep.getGener(), rep.getPetOwner(), rep.getPhonenumber(),rep.getBreed(), rep.getEmail(), rep.getInstagram(), rep.getDateOfLost(), rep.getCity(),rep.getDescription(), rep.getImageUrl()))
-            .collect(Collectors.toList());
-}
+//         //Modificación de nombre de atributos Getter
+//         return reportes.stream()
+//             .map(rep -> new ReporterDto(rep.getId(), rep.getPetName(), rep.getGener(), rep.getPetOwner(), rep.getPhonenumber(),rep.getBreed(), rep.getEmail(), rep.getInstagram(), rep.getDateOfLost(), rep.getCity(),rep.getDescription(), rep.getImageUrl()))
+//             .collect(Collectors.toList());
+// }
 //Cierre de llave faltante.
 }
 
