2436389d6969176d396a5ca96439804db9a6628e
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-10-01 02:22:23 -0500

    Comment register controller

--- petfinder/src/main/java/co/project/petfinder/Controller/Registercontroller.java
+++ petfinder/src/main/java/co/project/petfinder/Controller/Registercontroller.java
@@ -1,61 +1,61 @@
-package co.project.petfinder.Controller;
+// package co.project.petfinder.Controller;
 
 
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+// import org.springframework.stereotype.Controller;
+// import org.springframework.ui.Model;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.ModelAttribute;
+// import org.springframework.web.bind.annotation.PostMapping;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.web.bind.annotation.RequestMapping;
+// import org.springframework.web.bind.annotation.RequestMethod;
+// import org.springframework.web.bind.annotation.RestController;
 
-import co.project.petfinder.Controller.dto.RegisterDto;
-import co.project.petfinder.service.RegisterService;
-import lombok.AllArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-
-
-
-
-
-@RestController
-@RequestMapping("/register")
-@Slf4j
-@AllArgsConstructor
-public class Registercontroller {
-
-  private RegisterService registerService;
+// import co.project.petfinder.Controller.dto.RegisterDto;
+// import co.project.petfinder.service.RegisterService;
+// import lombok.AllArgsConstructor;
+// import lombok.extern.slf4j.Slf4j;
 
 
 
 
-  @PostMapping("/register")
-  public String postRegister(@ModelAttribute RegisterDto registerinfo, Model model) {
-      log.info(registerinfo.toString());
+
+
+// @RestController
+// @RequestMapping("/register")
+// @Slf4j
+// @AllArgsConstructor
+// public class Registercontroller {
+
+//   private RegisterService registerService;
+
+
+
+
+//   @PostMapping("/register")
+//   public String postRegister(@ModelAttribute RegisterDto registerinfo, Model model) {
+//       log.info(registerinfo.toString());
       
-      registerService.save(registerinfo);
+//       registerService.save(registerinfo);
 
-      model.addAttribute("info", registerinfo);
-      return "redirect:/";
-  }
+//       model.addAttribute("info", registerinfo);
+//       return "redirect:/";
+//   }
 
-  @GetMapping("/register")
-  public String getRegister(@ModelAttribute RegisterDto registerinfo, Model model) {
+//   @GetMapping("/register")
+//   public String getRegister(@ModelAttribute RegisterDto registerinfo, Model model) {
       
       
-      registerService.save(registerinfo);
+//       registerService.save(registerinfo);
 
-      model.addAttribute("info", registerinfo);
-      return "redirect:/";
-  }
-
-
+//       model.addAttribute("info", registerinfo);
+//       return "redirect:/";
+//   }
 
 
-}
+
+
+// }
   // private RegisterService registerService;
   
   // public Registercontroller (RegisterService registerService) {
