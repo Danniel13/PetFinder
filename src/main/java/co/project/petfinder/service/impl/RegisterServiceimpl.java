cc44b9766ad44dbda28bccaa677d9d5ce4c5b43a
Danniel Gonzalez <danielgz.m@hotmail.com>
2022-10-03 21:31:41 -0500

    Add RegisterServiceImpl

---
+++ src/main/java/co/project/petfinder/service/impl/RegisterServiceimpl.java
@@ -0,0 +1,45 @@
+package co.project.petfinder.service.impl;
+
+
+import org.springframework.stereotype.Service;
+
+import co.project.petfinder.Controller.dto.RegisterDto;
+import co.project.petfinder.model.entity.Register;
+import co.project.petfinder.model.repository.RegisterRepository;
+import co.project.petfinder.service.RegisterService;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+@Service
+public class RegisterServiceimpl implements RegisterService {
+
+
+    private RegisterRepository registerRepository;
+    
+  //Method
+  @Override
+  public void save(RegisterDto register){
+
+    //Validar metodo por campos Id y Reporters.
+// REPORTER !! 
+  //   Register register = new Register(null, registerDto.getName(), registerDto.getLastname(), registerDto.getEmail(),
+  //       registerDto.getPassword(), null);
+  //       return registerRepository.save(register);
+
+  // }
+  
+
+  // FALTA PONER ID SE DEJA SIN ID POR SER AUTO
+  var entity = new Register();
+  entity.setName(register.getName());
+  entity.setEmail(register.getEmail());
+  entity.setLastname(register.getLastname());
+  entity.setPassword(register.getPassword());
+
+  registerRepository.save(entity);
+
+
+
+
+}
+}
